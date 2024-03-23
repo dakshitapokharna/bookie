dbd17070a9bfb4d82941c2ea5009f52005f83e97
dakshitapokharna <dakshitapokharna2006@gmail.com>
2024-03-23 15:48:25 +0530

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <div class="nav">
+      <a class="active" href="#home">
+        Home
+      </a>{" "}
+      <a>{""}</a>
+      <a href="#recommendations">Our Recommendations</a> <a>{""}</a>
+      <a href="#contact">Contact Us</a> <a>{""}</a>
+      <a href="#about">About</a> <a>{""}</a>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/TOP.js
@@ -0,0 +1,9 @@
+const scrollbutton = () => {
+  return (
+    <button onclick="topFunction()" id="myBtn" title="Go to top">
+      Top
+    </button>
+  );
+};
+
+export default scrollbutton;
